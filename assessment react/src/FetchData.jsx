135347ec853c9fea24d5bab157f2b237d2bb9e1d
Leticia Saraiva <leticia.saraiva013@gmail.com>
2024-09-22 23:12:17 -0300

    feat: fetch data from API

---
+++ assessment react/src/FetchData.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react"
+
+const FetchData = () => {
+    const [users, setUsers] = useState([])
+    const [comments, setComments] = useState([])
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const fetchUsers = async () => {
+        setLoading(true)
+        try{
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+            if(response.ok) {
+                const users = await response.json()
+                setUsers(users)
+            } else {
+                setError('Erro ao buscar dados')
+            }
+        } catch (e) {
+            setError('Erro no fetch', e)
+        }
+    }
+
+    const fetchComments = async () => {
+        setLoading(true)
+        try{
+            const response = await fetch('https://jsonplaceholder.typicode.com/comments?postId=${postId}')
+            if(response.ok) {
+                const comments = await response.json()
+                setComments(comments)
+            } else {
+                setError('Erro ao buscar dados')
+            }
+        } catch (e) {
+            setError('Erro no fetch', e)
+        }
+    }
+
+    const fetchPosts = async () => {
+        setLoading(true)
+        try{
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts/?userId=${userId}')
+            if(response.ok) {
+                const posts = await response.json()
+                setPosts(posts)
+            } else {
+                setError('Erro ao buscar dados')
+            }
+        } catch (e) {
+            setError('Erro no fetch', e)
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    return {
+        users,
+        comments,
+        posts,
+        loading,
+        error,
+    }
+}
