055e1b72fb6aaecbd43df6c0fb97ec00942ca412
Leticia Saraiva <leticia.saraiva013@gmail.com>
2024-09-23 22:55:31 -0300

    feat: add navigation buttons

--- assessment react/src/PostPage.jsx
+++ assessment react/src/PostPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Logo from '/logo.png'
 import FetchData from './FetchData';
 import Post from './components/Post';
@@ -8,6 +8,7 @@ import Comment from './components/Comment'
 const PostPage = () => {
   const { postId } = useParams();
   const { posts, comments, loading, error } = FetchData();
+  const navigate = useNavigate()
 
   if (loading) return <div><img src={Logo} className="logo react" alt="React logo" /></div>
     if (error) return <div>Erro: {error.message}</div>
@@ -16,7 +17,8 @@ const PostPage = () => {
   const postComments = comments.filter((comment) => comment.postId === parseInt(postId));
 
   return (
-    <div>
+    <div className='main-card'>
+        <button onClick={() => navigate(-1)}>Back</button>
         {post && <Post post={post} />}
         <h2>Comments</h2>
         {postComments.map((comment) => (
--- assessment react/src/UserPage.jsx
+++ assessment react/src/UserPage.jsx
@@ -4,10 +4,12 @@ import FetchData from './FetchData';
 import User from './components/User';
 import Post from './components/Post';
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const UserPage = () => {
     const { userId } = useParams()
     const { users, posts, loading, error } = FetchData()
+    const navigate = useNavigate()
 
     if (loading) return <div><img src={Logo} className="logo react" alt="React logo" /></div>
     if (error) return <div>Erro: {error.message}</div>
@@ -17,6 +19,7 @@ const UserPage = () => {
 
     return (
         <div className='main-card'>
+            <button onClick={() => navigate(-1)}>Back</button>
             {user && <User user={user} />}
             {userPosts.map((post) => (
                 <div key={post.id}>
