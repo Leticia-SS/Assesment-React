e18eb60adae305295b7681439c0aaf973defaf0a
Leticia-SS <leticia.saraiva013@gmail.com>
2024-09-24 07:36:00 -0300

    feat: ajust username

--- assessment react/src/components/Comment.jsx
+++ assessment react/src/components/Comment.jsx
@@ -2,21 +2,15 @@ import './components.css'
 
 const Comment = ({comment }) => {
 
-    const newName = (name) => {
-        const nameSplit = name.split(" ")
-        if (nameSplit.lenght >=2) {
-            const first = nameSplit[0]
-            const last = nameSplit[nameSplit.lenght - 1]
-            return `${first}@${last}`
-        } else {
-            return `${nameSplit[0]}`
-        }
+    const username = (email) => {
+        const emailSplit = email.split("@")
+        return `@${emailSplit[0]}`
     }
 
     return (
         <div className='comment-card'>
             <h2>{comment.name}</h2>
-            <p>{comment.email}</p>
+            <p>{username(comment.email)}</p>
             <p>{comment.body}</p>
         </div>
     )
