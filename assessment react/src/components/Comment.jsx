654f72cf570b77d0e51472db70341b362728cc78
Leticia-SS <leticia.saraiva013@gmail.com>
2024-09-24 07:38:27 -0300

    feat: fiz name and username from comments section

--- assessment react/src/components/Comment.jsx
+++ assessment react/src/components/Comment.jsx
@@ -2,6 +2,16 @@ import './components.css'
 
 const Comment = ({comment }) => {
 
+    const newName = (name) => {
+        const nameSplit = name.split(" ")
+        if (nameSplit.lenght >2) {
+            const first = nameSplit[0]
+            const last = nameSplit[nameSplit.lenght - 1]
+            return `${first} ${last}`
+        }
+        return name
+    }
+
     const username = (email) => {
         const emailSplit = email.split("@")
         return `@${emailSplit[0]}`
@@ -9,7 +19,7 @@ const Comment = ({comment }) => {
 
     return (
         <div className='comment-card'>
-            <h2>{comment.name}</h2>
+            <h2>{newName(comment.name)}</h2>
             <p>{username(comment.email)}</p>
             <p>{comment.body}</p>
         </div>
