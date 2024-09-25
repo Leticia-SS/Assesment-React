31b4fa5ca40a680c95f98f9bd8870da8d2ca8657
Leticia Saraiva <leticia.saraiva013@gmail.com>
2024-09-25 00:13:44 -0300

    style: styling main page

--- assessment react/src/main.jsx
+++ assessment react/src/main.jsx
@@ -7,20 +7,27 @@ import Footer from './components/Footer'
 import './main.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import { faBorderAll, faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react'
 
 const Main = () => {
     const { users, loading, error } = FetchData()
+    const [ gridLayout, setGridLayout ] = useState(true)
 
     if (loading) return <div className='logo-container'><img src={Logo} className="logo react" alt="React logo" /></div>
     if (error) return <div>Erro: {error.message}</div>
+
+    const toggleGridLayout = () => {
+        setGridLayout(!gridLayout)
+    }
 
     return (
         <div className='landing-page-container'>
         <Header/>
         <div className='main-card'>
             <div className='button-container'>
-                <button className='grid-btn'><FontAwesomeIcon icon={faBorderAll} /></button>
+                <button className='grid-btn' onClick={toggleGridLayout}><FontAwesomeIcon icon={gridLayout ? faBarsStaggered : faBorderAll} /></button>
             </div>
+            <div className={gridLayout ? 'grid-layout' : 'flex-layout'}>
             {users.map((user)=>(
                 <div key={user.id}>
                     <Link to={`/users/${user.id}`}>
@@ -28,6 +35,7 @@ const Main = () => {
                     </Link>
                 </div>
             ))}
+            </div>
         </div>
         <Footer/>
         </div>
